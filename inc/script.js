ba8c92cfc8ecaad0e0a120cc55b38e804e5fe116
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-07-25 21:27:11 +0530

    carousel

--- inc/script.js
+++ inc/script.js
@@ -1,28 +1,25 @@
-$(document).ready(function () {
-  $(".carousel-wrapper").owlCarousel({
-    items: 3, // Number of items shown in the carousel at once
-    loop: true, // Enable looping of carousel items
-    autoplay: true, // Enable autoplay
-    autoplayTimeout: 3000, // Autoplay interval in milliseconds (3 seconds in this case)
-    autoplayHoverPause: true, // Pause autoplay when hovering over the carousel
-    // Add any other Owl Carousel options you need here
-  });
-});
-
-$(document).ready(function () {
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    items: 3,
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-  });
-  $(".play").on("click", function () {
-    owl.trigger("play.owl.autoplay", [3000]);
-  });
-  $(".stop").on("click", function () {
-    owl.trigger("stop.owl.autoplay");
-  });
-});
+$('.owl-carousel').owlCarousel({
+  stagePadding: 50,/*the little visible images at the end of the carousel*/
+  loop: true,
+  rtl: false,
+  lazyLoad: true,
+  margin: 0,
+  nav: true,
+  responsive: {
+    0: {
+      items: 2
+    },
+    600: {
+      items: 2
+    },
+    800: {
+      items: 3
+    },
+    1000: {
+      items: 2
+    },
+    1200: {
+      items: 3
+    }
+  }
+})
